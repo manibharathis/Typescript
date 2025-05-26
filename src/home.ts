afccf2a06712f6d6bda938ddd2a77529cae0676b
manibharathis <remasekar.96@gmail.com>
2025-05-26 16:05:26 +0530

    added any type

---
+++ src/home.ts
@@ -0,0 +1,9 @@
+let sales1 : string = "mani";
+let course : string = "typescpt";
+let sales : number = 12345_6789;
+let isCounted : boolean = true;
+
+function render (document:string) {
+console.log(document)
+}
+
